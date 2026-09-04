b37067a6e32319b49bf26fcaa4a32554cebbe85c
Suraj Mishra <72648167+sm5689@users.noreply.github.com>
2026-09-03 17:13:45 -0700

    Add: course-schedule - submission-2

---
+++ Data Structures & Algorithms/course-schedule/submission-2.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        queue<int> q;
+        vector<int> inorder(numCourses, 0);
+        unordered_map<int, vector<int>> adj;
+        for (int i = 0; i< prerequisites.size(); i++) {
+            int u = prerequisites[i][0];
+            int v = prerequisites[i][1];
+            inorder[v]++;
+            adj[u].push_back(v);
+        }
+        int cnt = 0;
+        for (int i = 0; i< numCourses; i++) {
+            if (inorder[i] == 0) {
+                q.push(i);
+            }
+        }
+        while(!q.empty()) {
+            int front = q.front();
+            q.pop();
+            cnt++;
+            for (int i = 0; i< adj[front].size(); i++) {
+                inorder[adj[front][i]]--;
+                if (inorder[adj[front][i]] == 0) q.push(adj[front][i]);
+            }
+        }
+        return cnt == numCourses;
+    }
+};
